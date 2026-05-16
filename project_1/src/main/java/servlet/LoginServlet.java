0b2a861946c4c44f6ffa1567d3cf138ecda5b69c
Longjing Jiang <longjinj@uci.edu>
2026-05-16 16:14:53 -0700

    remove idea files

--- project_1/src/main/java/servlet/LoginServlet.java
+++ project_1/src/main/java/servlet/LoginServlet.java
@@ -13,6 +13,7 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String email = request.getParameter("email");
+        String password = request.getParameter("password");
     }
 
 }
