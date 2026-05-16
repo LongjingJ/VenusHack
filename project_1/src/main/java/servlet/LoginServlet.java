8cde5977fe8c24a9ae924b228f3f5eacba60cd1d
Longjing Jiang <longjinj@uci.edu>
2026-05-16 15:21:32 -0700

    added dependencies

--- project_1/src/main/java/servlet/LoginServlet.java
+++ project_1/src/main/java/servlet/LoginServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/login")
+@WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
