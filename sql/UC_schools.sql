4c6c15afd7453531979df033d3ae7b99be8130cb
Longjing Jiang <longjinj@uci.edu>
2026-05-17 10:30:27 -0700

    added sql file

---
+++ sql/UC_schools.sql
@@ -0,0 +1,26 @@
+INSERT INTO schools(name, type)
+VALUES ('UC Berkeley', 'UC');
+
+INSERT INTO schools(name, type)
+VALUES ('UCLA', 'UC');
+
+INSERT INTO schools(name, type)
+VALUES ('UC Santa Barbara', 'UC');
+
+INSERT INTO schools(name, type)
+VALUES ('UC San Diego', 'UC');
+
+INSERT INTO schools(name, type)
+VALUES ('UC Irvine', 'UC');
+
+INSERT INTO schools(name, type)
+VALUES ('UC Davis', 'UC');
+
+INSERT INTO schools(name, type)
+VALUES ('UC Santa Cruz', 'UC');
+
+INSERT INTO schools(name, type)
+VALUES ('UC Riverside', 'UC');
+
+INSERT INTO schools(name, type)
+VALUES ('UC Merced', 'UC');
